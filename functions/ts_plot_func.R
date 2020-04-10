fc6cf43ce65506ca060a307550fd5dae67712350
weinbergerlab <dweinber@gmail.com>
2020-04-10 15:23:17 -0400

    update data, clean up code

--- functions/ts_plot_func.R
+++ functions/ts_plot_func.R
@@ -1,4 +1,4 @@
-ts.plot.func <-function(ds.plot=jh3, death.var='deaths' ,states.plot=states.cdc ){
+ts.plot.func <-function(ds.plot=jh3,ylim.adj=0.7, death.var='deaths' ,states.plot=states.cdc ){
   
   max.test.all.state<-max(ds.plot$test.week.per.capita, na.rm=T)
  
@@ -10,12 +10,12 @@ for(i in plot.state.indices){
   ds.select <- ds.plot[ds.plot$state==states.cdc[i],]
   max.test.this.state <- max(ds.select$test.week.per.capita, na.rm=T)
   ave_pi <- mean(ds.select$total_pi, na.rm=T)
-  ave.range <- c(-ave_pi*0.2, ave_pi*0.7)
+  ave.range <- c(-ave_pi*0.2, ave_pi*ylim.adj)
   y.range1<-range(c(ds.select[,death.var], (ds.select$excess_pi/ds.select$percent_complete)), na.rm=T)
   ds.select$death.early <- ds.select[,death.var]
   ds.select$death.early[is.na(ds.select$excess_pi)] <- NA
   par(new=FALSE)
-  plot(ds.select$date         ,
+  plot(ds.select$date+6         ,
        ds.select[,death.var],
        type='l',
        col='#377eb8',
@@ -23,16 +23,16 @@ for(i in plot.state.indices){
        bty='l',
        lty=3,
        xlab='',
-       ylab='N Deaths',
+       ylab='',
        #main=paste(states.plot[i] )
        )
   abline(h=0, col='grey', lty=2)
   
-  points(ds.select$date         ,
+  points(ds.select$date+6         ,
          ds.select$death.early, type='l', col='#377eb8',
          lty=1, lwd=1)
   
-  points(ds.select$date         ,
+  points(ds.select$date+6         ,
          ds.select$excess_pi/ds.select$percent_complete, type='l', col='#e41a1c', lwd=1)
   #state.name.plot <-  state.name[match(states.cdc[i],state.abb)]
   if(states[i] %in% state.abb ){
@@ -49,17 +49,17 @@ for(i in plot.state.indices){
  cl.min <- apply(pis,1, min) 
  cl.min <- cl.min[!is.na(cl.min)]
  date.ci <- ds.select$date[1:length(cl.min)]
-  polygon(c(date.ci, rev(date.ci)),
+  polygon(c(date.ci+6, rev(date.ci+6)),
           c(cl.min, rev(cl.max)),
           col = rgb(1, 0,0, alpha = 0.05), 
           border = NA)
   
   scale.factor.test <- ave.range[2]/max.test.all.state
-  points(ds.select$date, scale.factor.test*ds.select$test.week.per.capita, type='l', lty=3, lwd=1, col='gray', yaxt='n',xaxt='n', ylab='', xlab='')
+  points(ds.select$date+6, scale.factor.test*ds.select$test.week.per.capita, type='l', lty=3, lwd=1, col='gray', yaxt='n',xaxt='n', ylab='', xlab='')
   
   axis(side=4,at=(c(0,2,4,6)*(scale.factor.test)) , labels=c(0,2,4,6), col.ticks='gray', col='gray', col.lab='gray' ,col.axis='gray')
-  mtext("Tests/1000", side=4, line=2, col='gray', cex=0.75)
-  text(as.Date('2020-02-06'), ave.range[2]*0.9,state.name.plot, pos=4, cex=0.85)
+  mtext("", side=4, line=2, col='gray', cex=0.75)
+  text(as.Date('2020-02-06')+6, ave.range[2]*0.9,state.name.plot, pos=4, cex=0.85)
   
   box(lty = '1111', col = 'lightgray')
 }
