235c78ddcc720a36be68ac6aec7e866e9b513dcd
weinbergerlab <dweinber@gmail.com>
2020-05-26 11:48:07 -0400

    plotting all cause deaths in fig 4; make the analysis of provisional deaths more automatic

--- functions/ts_plot_func.R
+++ functions/ts_plot_func.R
@@ -1,4 +1,4 @@
-ts.plot.func <-function(ds.plot=jh3,ylim.adj=0.7, death.var='deaths' ,states.plot=states.cdc , plot.order=F ){
+ts.plot.func <-function(ds.plot=jh3,ylim.adj=0.7, death.var='deaths' ,states.plot=states.cdc , plot.order=F,plot.excess='pic' ){
   
   max.test.all.state<-max(ds.plot$test.week.per.capita[ds.plot$state %in% states.plot], na.rm=T)
  
@@ -11,16 +11,18 @@ for(i in 1:4){
   ds.select <- ds.plot[ds.plot$state==states.cdc[i],]
   max.test.this.state <- max(ds.select$test.week.per.capita, na.rm=T)
   ave_pi <- mean(ds.select$pneumonia_influenza_covid, na.rm=T)
+  if(plot.excess=='pic'){
   y.range1<-range(c(ds.select[,death.var], (ds.select$excess_pneumonia_influenza_covid)), na.rm=T)
-  
+  }else{
+    y.range1<-range(c(ds.select[,death.var], (ds.select$excess_all_cause_death)), na.rm=T)
+    
+  }
   #Makes all have same relative range
   #ave.range <- c(-ave_pi*0.2, ave_pi*ylim.adj)
   
   ave.range <-y.range1 
   
-  ds.select$death.early <- ds.select[,death.var]
-  ds.select$death.early[is.na(ds.select$excess_pneumonia_influenza_covid)] <- NA
-  par(new=FALSE)
+   par(new=FALSE)
   plot(ds.select$week_end_date         ,
        ds.select[,death.var],
        type='l',
@@ -34,21 +36,33 @@ for(i in 1:4){
        )
   abline(h=0, col='grey', lty=2)
   
-  points(ds.select$week_end_date        ,
-         ds.select$death.early, type='l', col='#377eb8',
-         lty=1, lwd=1)
-  
+  if(plot.excess=='pic'){
+    
   points(ds.select$week_end_date        ,
          ds.select$excess_pneumonia_influenza_covid, type='l', col='#e41a1c', lwd=1)
-  #state.name.plot <-  state.name[match(states.cdc[i],state.abb)]
+  }else{
+    points(ds.select$week_end_date        ,
+           ds.select$excess_all_cause_deaths, type='l', col='#e41a1c', lwd=1)
+    
+  }
+    
+    #state.name.plot <-  state.name[match(states.cdc[i],state.abb)]
   if(states[i] %in% state.abb ){
     state.name.plot <-    
       state.name[match(states.cdc[i],state.abb)]
   }else{
     state.name.plot <- states.cdc[i]
   }
+  
+  if(plot.excess=='pic'){
  upper <-  ds.select$pneumonia_influenza_covid  - ds.select$baseline_pi_upper  
  lower <-  ds.select$pneumonia_influenza_covid - ds.select$baseline_pi_lower 
+  }else{
+ upper <-  ds.select$all_cause_deaths  - ds.select$baseline_all_cause_lower  
+ lower <-  ds.select$all_cause_deaths - ds.select$baseline_all_cause_upper 
+    
+  }
+ 
  pis <- cbind(upper, lower)
  cl.max <- apply(pis,1, max) 
  cl.max <-cl.max[!is.na(cl.max)]
@@ -68,7 +82,14 @@ for(i in 1:4){
   
   axis(side=4,at=(c(0,4, 8,  12)*(scale.factor.test)) , labels=c(0,4, 8,  12), col.ticks='gray', col='gray', col.lab='gray' ,col.axis='gray')
   mtext("", side=4, line=2, col='gray', cex=0.75)
-  text(as.Date('2020-02-01')+6, ave.range[2]*0.9,state.name.plot, pos=4, cex=0.85)
+  text(as.Date('2020-03-01')+6, ave.range[2]*0.9,state.name.plot, pos=4, cex=0.85)
+  
+  if(i==1){
+    legend(as.Date('2020-03-05'),1000, legend=c('Excess deaths','COVID-19 deaths', 'Tests'), 
+           col=c('#e41a1c','#377eb8','gray'),
+           inset=0.01, lty=c(1,3,2),box.lty=0 ,lwd=c(1,1,2),
+           bg="transparent", cex = 0.75)
+  }
   
   box(lty = '1111', col = 'lightgray')
 }
